e9c44be3ed396e15b5bc1d3d7667ae243643783a
Selbin <selbinzr@gmail.com>
2018-07-04 15:06:30 +0530

    loaded html page using fs module

---
+++ fileOperaion/fileOperation.js
@@ -0,0 +1,43 @@
+const http = require('http')
+const url = require('url')
+const path = require('path')
+const fs = require('fs')
+
+
+const mimeTypes = {'html' : 'text/html' , 'jpeg' : 'image/jpeg' , 'js' : 'text/javascript','css' : 'text/css'}
+
+
+http.createServer(function(req,res){
+
+    var uri = url.parse(req.url).pathname
+    var fileName =path.join(process.cwd(),unescape(uri))
+    console.log('loading....'+ uri)
+    var stats 
+    try {
+        stats = fs.lstatSync(fileName)
+    } catch (e) {
+        res.writeHead(404 , {'content': 'text/plain'})
+        res.write('404 not found')
+        res.end()
+        return
+    }
+
+
+    if(stats.isFile()){
+        var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]]
+        res.writeHead( 200 ,{ 'content-type' : 'mimeType'})
+        var fileStream = fs.createReadStream(fileName)
+        fileStream.pipe(res)
+
+
+
+    }else if(stats.isDirectory()){
+
+        res.writeHead(302 ,{'location' : 'index.html'})
+        res.end()
+    }else{
+        writeHead(500, {'content-type' : 'text/plain'})
+        res.write('500 internal error')
+
+    }
+}).listen(3000)
